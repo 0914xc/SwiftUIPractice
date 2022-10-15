a4121c920aa89a533940187eda3434031fae4739
0914xc <weixiaochen0914@gmail.com>
2022-10-15 18:22:09 +0800

    init project

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by weixiaochen on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
